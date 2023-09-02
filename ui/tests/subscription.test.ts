192c4de1f282fbafe8e531118b11109f1d7f198e
Mikhail Kudryashov <mikhail.kudryashov@orange-city.ru>
2023-09-02 15:49:01 +0400

    subscription test

---
+++ ui/tests/subscription.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { DataLayer } from '@Utils/dataLayer';
+
+test.describe('check subscription event in Datalayer', () => {
+    test.beforeEach(async ({ page, baseURL }) => {
+        await page.context().addCookies([
+            {
+                name: 'OptanonAlertBoxClosed',
+                value: new Date().toISOString(),
+                url: baseURL,
+            },
+        ]);
+        await page.goto('/', { waitUntil: 'domcontentloaded' });
+    });
+    test('event should fire after click sign up button', async ({ page, baseURL }) => {
+        const dataLayer = new DataLayer(page);
+        const expectedEvent = {
+            event: 'GeneralInteraction',
+            eventCategory: 'Footer - D',
+            eventAction: 'Newsletter Subscription',
+            eventLabel: 'Success',
+        };
+
+        const emailInput = page.locator('//footer//input');
+        await emailInput.fill('test@mail.ru');
+
+        const signUpButton = page.locator('//footer//button[contains(., "Sign Up" )]');
+        await signUpButton.click();
+
+        const [event] = await dataLayer.waitForDataLayer({
+            event: 'GeneralInteraction',
+            eventCategory: 'Footer - D',
+            eventAction: 'Newsletter Subscription',
+        });
+
+        expect(event).toStrictEqual(expectedEvent);
+    });
+});
